6eb85fb08f5afb182e16ffe95015a71dedf84667
luisgustavopn <luisgustavop776@gmail.com>
2023-06-26 18:36:13 -0300

    pegando API das UFS - IBGE e instalação do thunder client

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const api = axios.create({
+	baseURL: "https://servicodados.ibge.gov.br/api/v1/localidades/estados/",
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const AppNavBar = () => {
+	return (
+		<div className="card">
+			<h1>Lista de tarefas (apenas leitura)</h1>
+		</div>
+	);
+};
 
-export default App
+const AppTarefas = () => {
+	const [tarefas, setTarefas] = useState([]);
+
+	const tratarClique = () => {
+		api.get("pegar").then((response) => {
+			console.info(response.data);
+			const lista = response.data.data.map((item: any) =>  item.sigla);
+			console.info(lista);
+			setTarefas(lista);
+		});
+	};
+	return (
+		<div className="card">
+			<button onClick={tratarClique}>Pegar tarefas</button>
+			<ul>
+				{tarefas.map((tarefa: string, indice: number) => (
+					<li key={indice}>{tarefa}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+const App = () => {
+	return (
+		<>
+			<AppNavBar />
+			<AppTarefas />
+		</>
+	);
+};
+
+export default App;
